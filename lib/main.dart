a913659c5de3acf4e3268d8c8d3b18d928f4576f
Yuvraj-Lolage <yuvilolage7@gmail.com>
2024-09-08 13:20:15 +0530

    added splash screeen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 import 'package:flutter_language_translator/pages/home.dart';
+import 'package:flutter_language_translator/pages/splash_screen.dart';
 import 'package:flutter_language_translator/themes/theme.dart';
 
 void main() {
   runApp(MaterialApp(
     theme: darkTheme,
-    initialRoute: '/home',
+    initialRoute: '/',
     routes: {
+      '/': (context) => SplashScreen(),
       '/home': (contex) => Home(),
     },
   ));
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,11 +8,36 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacementNamed('/home');
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      backgroundColor: Colors.tealAccent[400],
+      body: Center(
+          child: Container(
+        decoration: BoxDecoration(color: Colors.tealAccent[400]),
+        child: const Text(
+          'Translation App',
+          style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+        ),
+      )),
     );
   }
 }
